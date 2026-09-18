aa614e39a605d2e022ba14f0f138f4d7279d93ce
Anusha Reddy Dasari <anushareddydasari16@gmail.com>
2026-09-18 00:17:49 -0400

    add

--- test.py
+++ test.py
@@ -4,17 +4,17 @@ from tools.tavily_tool import tavily_search
 # print(res)
 
 from tools.flight_tool import search_flights
-res = search_flights ("Plan a 7 days IND  trip from BOS")
-print(res)
+# res = search_flights ("Plan a 7 days IND  trip from BOS")
+# print(res)
 
-# from backend import run_travel_agent
+from backend import run_travel_agent
 
-# user_input = input("Enter travel request: ")
+user_input = input("Enter travel request: ")
 
-# response = run_travel_agent(
-#     user_input=user_input,
-#     thread_id="test_user"
-# )
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
 
-# print("\nFINAL RESPONSE:\n")
-# print(response["answer"])
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
